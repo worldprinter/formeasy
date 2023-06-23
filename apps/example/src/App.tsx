75dc05faea4b133da44e708c7b71179863497e41
KurumiRin <1548627528@qq.com>
2023-06-23 16:43:58 +0800

    fix: fixed logic

--- apps/example/src/App.tsx
+++ apps/example/src/App.tsx
@@ -1,72 +1,111 @@
+import { set } from 'lodash'
 import React from 'react'
 
 import { Field, FormikProvider, Logic, useFormik } from '@worldprinter/formeasy'
 
 function Test(props: Record<string, unknown>) {
-    console.log(props)
-    return <div>123</div>
+  console.log(props)
+  return <div>123</div>
 }
 
+function Select({data,value}:any){
+  return (
+    <select>
+      {
+        data.map((i:any)=>{
+          return (
+            <option selected={value === i.value}  value={i.value}>
+              {i.label}
+            </option>
+          )
+        })
+      }
+    </select>
+  )
+}
+
+// window.__DEV__ = true
+set(window, '__DEV__', true)
+
 function App() {
-    const formik = useFormik({
-        initialValues: {
-            firstName: 'xn',
-            lastName: '',
-            email: '',
-        },
-        onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2))
-        },
-        watch: {
-            lastName: (value, formValue) => {
-                formValue.firstName = value + 'xxx'
-                // formValue.firstName =  value + 'xxx'
-            },
-        },
-    })
+  const formik = useFormik({
+    initialValues: {
+      firstName: 'xn',
+      lastName: '',
+      email: '',
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    },
+    watch: {
+      lastName: (value, formValue) => {
+        formValue.firstName = value + 'xxx'
+        // formValue.firstName =  value + 'xxx'
+      },
+    },
+  })
 
-    React.useEffect(() => {
-        console.log('values: ', formik.values)
-    }, [formik.values])
+  React.useEffect(() => {
+    console.log('values: ', formik.values)
+  }, [formik.values])
 
-    return (
-        <div>
-            <div>123</div>
-            <FormikProvider value={formik}>
-                <form onSubmit={formik.handleSubmit}>
-                    <Logic
-                        name={'email'}
-                        exp={'form.values.lastName.length > 5'}
-                        expTrue={{ a: 1 }}
-                        // expFalse={{ a: 2 }}
-                    >
-                        <Test
-                            a='3'
-                            b='4'
-                        />
-                    </Logic>
-                    <Field
-                        type='text'
-                        name='lastName'
-                        placeholder='lastName'
-                    />
-                </form>
-            </FormikProvider>
-            <button
-                onClick={() =>
-                    formik.setValues({
-                        ...formik.values,
-                        firstName: '123',
-                        lastName: '123',
-                        email: '123',
-                    })
-                }
-            >
-                test
-            </button>
-            'firstName' {formik.values.firstName}
-        </div>
-    )
+  return (
+    <div>
+      <div>123</div>
+      <FormikProvider value={formik}>
+        <form onSubmit={formik.handleSubmit}>
+          <Logic
+            // name={'email'}
+            // exp={'form.values.lastName.length > 5'}
+            // expTrue={
+            //   {
+            //     data:[
+            //       {
+            //         label:3,
+            //         value:3
+            //       },
+            //       {
+            //         label:4,
+            //         value:4
+            //       }
+            //     ]
+            //   }
+            // }
+            // expFalse={{ a: 2 }}
+          >
+            <Select data={[
+              {
+                label:1,
+                value:1
+              },
+              {
+                label:2,
+                value:2
+              }
+            ]}/>
+          </Logic>
+          <Field
+            type='text'
+            name='lastName'
+            placeholder='lastName'
+          />
+        </form>
+      </FormikProvider>
+      <button
+        onClick={() =>
+          formik.setValues({
+            ...formik.values,
+            firstName: '123',
+            lastName: '123',
+            email: '123',
+          })
+        }
+      >
+        test
+      </button>
+      'firstName' {formik.values.firstName}
+    </div>
+  )
 }
 
 export default App
--- libs/formeasy/src/components/Logic.tsx
+++ libs/formeasy/src/components/Logic.tsx
@@ -6,42 +6,47 @@ import type { FieldAttributes } from '../formik'
 import { useField, useFormikContext } from '../formik'
 
 export type LogicFieldProps<T> = {
-    exp: string
-    expTrue?: Partial<T>
-    expFalse?: Partial<T>
+  exp?: string
+  expTrue?: Partial<T>
+  expFalse?: Partial<T>
 } & FieldAttributes<T>
 
 export const Logic = React.memo(function LogicField<T>(props: React.PropsWithChildren<LogicFieldProps<T>>) {
-    const hookProps = React.useMemo(() => {
-        // eslint-disable-next-line
-        // @ts-ignore
-        return omit(props, ['exp', 'expTrue', 'expFalse', 'children']) as FieldAttributes<T>
-    }, [props])
-    const { exp, expTrue, expFalse } = props
+  const hookProps = React.useMemo(() => {
+    // eslint-disable-next-line
+    // @ts-ignore
+    return omit(props, ['exp', 'expTrue', 'expFalse', 'children']) as FieldAttributes<T>
+  }, [props])
+  const { exp, expTrue, expFalse } = props
 
-    const form = useFormikContext()
-    const [field, meta, helper] = useField(hookProps)
-    const [logicProps, setLogicProps] = React.useState<FieldAttributes<T>>(hookProps as FieldAttributes<T>)
+  const form = useFormikContext()
+  const [field, meta, helper] = useField(hookProps)
+  const [logicProps, setLogicProps] = React.useState<FieldAttributes<T>>(hookProps as FieldAttributes<T>)
 
-    React.useEffect(() => {
-        const { result } = expression(exp, {
-            field,
-            meta,
-            helper,
-            form,
-        })
+  React.useEffect(() => {
+    if(!exp || !exp.trim().length){
+      setLogicProps({...hookProps} as FieldAttributes<T>)
+      return
+    }
 
-        if (result) {
-            setLogicProps({ ...hookProps, ...(expTrue || {}) } as FieldAttributes<T>)
-        } else {
-            setLogicProps({ ...hookProps, ...(expFalse || {}) } as FieldAttributes<T>)
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [exp, expFalse, expTrue, form, hookProps])
+    const { result } = expression(exp, {
+      field,
+      meta,
+      helper,
+      form,
+    })
 
-    return React.cloneElement(
-        // eslint-disable-next-line
-        props.children as React.ReactElement<any>,
-        logicProps,
-    )
+    if (result) {
+      setLogicProps({ ...hookProps, ...(expTrue || {}) } as FieldAttributes<T>)
+    } else {
+      setLogicProps({ ...hookProps, ...(expFalse || {}) } as FieldAttributes<T>)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [exp, expFalse, expTrue, form, hookProps])
+
+  return React.cloneElement(
+    // eslint-disable-next-line
+    props.children as React.ReactElement<any>,
+    logicProps,
+  )
 })
